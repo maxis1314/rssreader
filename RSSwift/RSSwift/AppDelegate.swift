bb78e87218a50313cfd2931e6f1dd5b2e7e4fadf
Hongping Tang <hongping.tang@ext.upa.com>
2018-01-12 14:57:52 +0800

    daniel:

--- RSSwift/RSSwift/AppDelegate.swift
+++ RSSwift/RSSwift/AppDelegate.swift
@@ -25,49 +25,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         
         
-        let contactIonfo = NSEntityDescription.insertNewObject(forEntityName: "EagleList", into: managedObjectContext) as! EagleList
-        
-        //两种赋值方式 如果你创建了Preson 的NSManagedObjectModel
-        //那就可以用点语法调出属性否则只能用setValue赋值
-        
-        //点语法赋值
-        contactIonfo.url = "ddd"
-        contactIonfo.name = "wangmumu"
-        
-        //setValue赋值
-        //contactIonfo .setValue("1", forKey: "name")
-        //contactIonfo .setValue(11, forKey: "age")
-        
-        ///  保存到本地
-        saveContext()
+        save_eagle(title:"a",url:"http://a.com")
+        save_eagle(title:"b",url:"http://b.com")
+        save_eagle(title:"c",url:"http://c.com")
         
         
         
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>.init()
-        ///  生成一个要查询的表的对象
-        let entity = NSEntityDescription.entity(forEntityName: "EagleList", in: managedObjectContext)
-        ///  查询对象属性
-        fetchRequest.entity = entity
-        ///  判断查询对象是否为空 防止崩溃
-        if (entity != nil) {
-            ///  查询结果
-            do{
-                /// 成功
-                let qwqwrr:[AnyObject]?  = try managedObjectContext.fetch(fetchRequest)
-                for info:NSManagedObject in qwqwrr as![NSManagedObject] {
-                    print(info)
-                }
-            }catch{
-                /// 失败
-                fatalError("查询失败：\(error)")
-            }
-        }else{
-            ///  查询对象不存在
-            print("查询失败：查询不存在")
-        }
-        
- 
- 
+        print(eagle_list())
         
         return true
     }
