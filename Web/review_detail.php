faec9069bcefcb3be523d617d5f5fb59dc4ffc57
Hongping Tang <hongping.tang@ext.upa.com>
2018-01-17 17:53:25 +0800

    daniel:addrss

--- Web/review_detail.php
+++ Web/review_detail.php
@@ -3,33 +3,45 @@ error_reporting(0);
 require("data/MysqlDAO.php");
 require("data/dbcenter.php");
 
-//check_sign();
+check_sign();
 
 $id = get("review_id");
 
- header("Content-Type: application/rss+xml; charset=utf-8");  
+//header("Content-Type: application/rss+xml; charset=utf-8");  
 
  $db = get_db();
- $data = $db->get_list_h("select * from rss where id='$id'");
+ $data = $db->get_one_h("select * from rss where id='$id'");
  
- $rssfeed = '<?xml version="1.0" encoding="utf-8"?>';
- $rssfeed .= '<rss version="2.0">';
- $rssfeed .= '<channel>';
- $rssfeed .= '<title>My RSS feed</title>';
- $rssfeed .= '<link>http://www.mywebsite.com</link>';
- $rssfeed .= '<description>This is an example RSS feed</description>';
- $rssfeed .= '<language>en-us</language>';
- $rssfeed .= '<copyright>Copyright (C) 2009 mywebsite.com</copyright>';  
-
- foreach($data as $one){
-     extract($row);  
-     $rssfeed .= '<item>';
-     $rssfeed .= '<title>' . $one['title'] . '</title>';
-     $rssfeed .= '<description>' . $one['description'] . '</description>';
-     $rssfeed .= '<link>' . $one['link'] . '</link>';
-     $rssfeed .= '<pubDate>' . date("D, d M Y H:i:s O", strtotime($one['date'])) . '</pubDate>';
-     $rssfeed .= '</item>';
- }  
- $rssfeed .= '</channel>';
- $rssfeed .= '</rss>';  echo $rssfeed;
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">
+.ui-page .ui-header {
+    background: #558C89 !important;
+}
+</style>
+
+<link rel="stylesheet" href="jquery.mobile-1.3.2.min.css">
+<script src="jquery-1.8.3.min.js"></script>
+<script src="jquery.mobile-1.3.2.min.js"></script>
+</head>
+<body>
+
+<div data-role="page">
+  <div data-role="header">
+    <h1><?php echo $data['title']; ?></h1>
+  </div>
+
+  <div data-role="content">
+    <p><?php echo $data['description']; ?></p>
+  </div>
+
+ <!-- <div data-role="footer">
+  <h1>页脚文本</h1>
+  </div>-->
+</div> 
+
+</body>
+</html>
