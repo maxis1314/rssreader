6bd4b4ab757afb7fbcc7d9629de8d8608d9f70e0
Hongping Tang <hongping.tang@ext.upa.com>
2018-01-18 13:14:32 +0800

    daniel:v1.3.9

--- RSSwift/RSSwift/DBManager.swift
+++ RSSwift/RSSwift/DBManager.swift
@@ -452,6 +452,28 @@ func parse_json(file:String) -> [String:AnyObject]{
     return nil
 }
 
+func save_to_file(file:String, text:String){
+    //let file = "file.txt" //this is the file. we will write to and read from it
+    //let text = "some text" //just a text
 
+    if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+
+        let fileURL = dir.appendingPathComponent(file)
+
+        //writing
+        do {
+            try text.write(to: fileURL, atomically: false, encoding: .utf8)
+        }
+        catch {/* error handling here */}
+
+        //reading
+        do {
+            let text2 = try String(contentsOf: fileURL, encoding: .utf8)
+        }
+        catch {/* error handling here */}
+    }
+}
+func read_from_file(file:String){
+}
 
 
