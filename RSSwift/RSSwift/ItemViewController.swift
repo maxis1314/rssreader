144017a58ee7b5f6c90745a7ecf67fb763fe29af
daniel <maxis1314@tangmatoMacBook-Pro.local>
2018-01-21 14:53:56 +0800

    daniel:v1.6.17addshare

--- RSSwift/RSSwift/ItemViewController.swift
+++ RSSwift/RSSwift/ItemViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ItemViewController: UIViewController {
 
+    @IBAction func shareTextButton(_ sender: Any) {
+        // text to share
+        let text = "This is some text that I want to share."
+        
+        // set up activity view controller
+        let textToShare = [ text ]
+        let activityViewController = UIActivityViewController(activityItems: textToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+        
+        // exclude some activity types from the list (optional)
+        activityViewController.excludedActivityTypes = [ UIActivityType.airDrop, UIActivityType.postToFacebook ]
+        
+        // present the view controller
+        self.present(activityViewController, animated: true, completion: nil)
+        
+    }
     @IBOutlet weak var leftBtn: UIButton!
 
     @IBOutlet weak var contentTxt: UITextField!
