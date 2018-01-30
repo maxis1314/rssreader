10154de12bd51559752ce4387657014c2fda216b
Hongping Tang <hongping.tang@ext.upa.com>
2018-01-30 15:58:31 +0800

    daniel:

--- RSSwift/RSSwift/FeedItemViewController.swift
+++ RSSwift/RSSwift/FeedItemViewController.swift
@@ -113,7 +113,7 @@ class FeedItemViewController: UIViewController, UIWebViewDelegate,GADBannerViewD
             var font = ddStorageGet(key: "font", empty:"20")
             print(font)
 
-            let finalStr = "<style>html{font-size: \(font)px;}</style>\(feed.description)"
+            let finalStr = "<style>html{font-size: \(font)px;}</style><h3>\(feed.title)</h3><hr>\(feed.description)<a href='\(feed.link)'>[Source]</a>"
             
             myWebView.scrollView.scrollsToTop = true
             myWebView.loadHTMLString(finalStr, baseURL: nil)
