f398aa07366d21945301e53e84e04723554017d2
daniel <maxis1314@tangmatoMacBook-Pro.local>
2018-02-03 22:56:23 +0800

    daniel:1

--- RSSwift/RSSwift/DBManager.swift
+++ RSSwift/RSSwift/DBManager.swift
@@ -19,7 +19,8 @@ class GDB{
 let gdb = GDB()
 let dbEagle = DBEagle()
 let dbFeed = DBFeed()
-let baseURL = "http://localhost"
+//let baseURL = "http://localhost"
+let baseURL = "http://wp.rabbit-e.com"
 
 /// 被管理的数据上下文   初始化的后，必须设置持久化存储助理
 var managedObjectContext: NSManagedObjectContext = {
